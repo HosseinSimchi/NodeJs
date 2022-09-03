0e3800f38239a2a1f49384f600317b01d9e4a37e
= <=>
2022-09-03 16:51:49 +0430

    we did everything until the Express JS

---
+++ index.js
@@ -0,0 +1,22 @@
+const chalk = require("chalk");
+// const fs = require('fs');
+// const yargs = require('yargs');
+
+
+// yargs.command({
+//     command : "CreateUser",
+//     aliases : ['CU'],
+//     builder : {
+//         name : {type: 'String'}
+//     },
+//     handler(argv){
+//         console.log(chalk.red(`Hello my name is ${argv.name}`));
+//     }
+// })
+
+// yargs.parse()
+
+// using with this :  node index.js CU --name "Hossein Simchi"
+
+
+
